2177662391f7c418ebccb377a6603de0d681ba13
Sanskar-Sarawgi <sanskar@mobicip.com>
2023-01-05 19:32:26 +0530

    delete file

--- Headernew/Filter.h
+++
@@ -1,7 +0,0 @@
-#include "Buffer.h"
-
-int Is_Tag(Cursor *, char *);
-
-void Blocking(Cursor *, char *);
-
-Buffer_node *Remove_Tag(Buffer_node *, Buffer_node *);
--- Headernew/Sort.h
+++
@@ -1,9 +0,0 @@
-typedef struct node
-{
-    int fre;
-    char *word;
-} node;
-
-int cmpfunc(const void *, const void *);
-
-void Sort_file(int , char *);
